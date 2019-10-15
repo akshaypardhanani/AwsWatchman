1f26660aa99b7c90e8ce45cd4949d7fea68fcb11
Akshay Pardhanani <akshay_p27393@ymail.com>
2019-10-15 16:25:43 -0500

    Added the SageMaker Metric Source file

---
+++ Watchman.AwsResources/Services/SageMaker/SageMakerSource.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.SageMaker;
+using Amazon.SageMaker.Model;
+
+namespace Watchman.AwsResources.Services.SageMaker
+{
+    public class SageMakerSource : ResourceSourceBase<EndpointSummary>
+    {
+        private readonly IAmazonSageMaker _amazonSageMaker;
+
+        public SageMakerSource(IAmazonSageMaker amazonSageMaker)
+        {
+            _amazonSageMaker = amazonSageMaker;
+        }
+
+        protected override string GetResourceName(EndpointSummary resource)
+        {
+            return resource.Id;
+        }
+
+        protected override async Task<IEnumerable<EndpointSummary>> fetchResources()
+        {
+            var results = new List<IEnumerable<EndpointSummary>>();
+            string nextToken = null;
+
+            do
+            {
+                var response = await _amazonSageMaker.ListEndpointsAsync(new ListEndpointsRequest()
+                {
+                    NextToken = nextToken
+                });
+
+                results.Add(response.Endpoints.ToList());
+                nextToken = response.NextToken;
+            } while (!string.IsNullOrEmpty(nextMarker));
+
+            return results.SelectMany(x => x).ToList();
+        }
+    }
+}
